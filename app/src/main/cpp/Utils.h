bafddb80a8fab89c239ebca4d4ed2952eb66ecee
dfqin <dfqin@hotmail.com>
2018-12-27 11:50:05 +0800

    添加签名和混淆

--- app/src/main/cpp/Utils.h
+++ app/src/main/cpp/Utils.h
@@ -38,11 +38,11 @@ extern bool isDebug;
 #define PACKAGE_FRONT "com.dfqin."//app packageName 前半部分
 #define PACKAGE_END "encrypt"  // app packageName 后半部分
 
-#define SIGN_FRONT_DEBUG "3082014602010130" //debug签名的第8至第15位
-#define SIGN_END_DEBUG "c46854c5f4b1aeb2"   //debug签名的倒数第15位至倒数第8位
+#define SIGN_FRONT_DEBUG "30820241a0030201" //debug签名的第8至第15位
+#define SIGN_END_DEBUG "f51a6037246c9182"   //debug签名的倒数第15位至倒数第8位
 
-#define SIGN_FRONT_RELEASE "308201bca0030201" //release签名的第8至第15位
-#define SIGN_END_RELEASE   "9cefd2ac8c1e9db6" //release签名的倒数第15位至倒数第8位
+#define SIGN_FRONT_RELEASE "30820241a0030201" //release签名的第8至第15位
+#define SIGN_END_RELEASE   "f51a6037246c9182" //release签名的倒数第15位至倒数第8位
 
 #define  LOGV(...)  __android_log_print(ANDROID_LOG_VERBOSE, "NTag", __VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR, "Ntag", __VA_ARGS__)
